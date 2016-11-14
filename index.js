241b37a5c0bb727a13267236bb663808d1cb465f
Pikachu <cameron.payton7@gmail.com>
2016-11-13 23:45:28 -0500

    Updated script to log in to the CS server properly

--- index.js
+++ index.js
@@ -1,17 +1,65 @@
-var page = require('webpage').create();
-//var creds = require("./config");
-page.open('https://grades.cs.umd.edu/classWeb/login.cgi', function(status) {
-  console.log("Status: " + status);
-  var body = page.evaluate(function() {
-     return document.body;
-  });
-  console.log(body.innerHTML);
-  /*
-  var nameEle = page.evaluate(function(){
-      var b = document.getElementsByName('name');
-      return b;
+var my_info = require('./config');
+var page = new WebPage(), testindex = 0, loadInProgress = false;
+
+page.onConsoleMessage = function(msg) {
+  console.log(msg);
+};
+
+page.onLoadStarted = function() {
+  loadInProgress = true;
+  console.log("load started");
+};
+
+page.onLoadFinished = function() {
+  loadInProgress = false;
+  console.log("load finished");
+};
+
+var steps = [
+  function() {
+    //Load Login Page
+    page.open("https://grades.cs.umd.edu/classWeb/login.cgi");
+  },
+  function() {
+    //Enter Credentials
+    page.evaluate(function(my_info) {
+
+      var arr = document.getElementsByTagName("form");
+      arr[0].elements["user"].value = my_info.username;
+      arr[0].elements["password"].value = my_info.password;
+      console.log(arr[0].elements["user"].value);
+      console.log(arr[0].elements["password"].value);
+    }, my_info);
+  },
+  function() {
+    //Login
+    page.evaluate(function() {
+
+      var arr = document.getElementsByTagName("form");
+      console.log(arr[0].elements["user"].value);
+      console.log(arr[0].elements["password"].value);
+      arr[0].submit.click();
+      console.log(arr[0]);
+
+    });
+  },
+  function() {
+    // Output content of page to stdout after form has been submitted
+    page.evaluate(function() {
+      console.log(document.querySelectorAll('html')[0].outerHTML);
+    });
   }
-  console.log(nameEle.length);
-  */
-  phantom.exit();
-});
+];
+
+
+interval = setInterval(function() {
+  if (!loadInProgress && typeof steps[testindex] == "function") {
+    console.log("step " + (testindex + 1));
+    steps[testindex]();
+    testindex++;
+  }
+  if (typeof steps[testindex] != "function") {
+    console.log("test complete!");
+    phantom.exit();
+  }
+}, 50);
