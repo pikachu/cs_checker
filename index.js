56c80f120f72c4de9b2565d471d0ca7e71739028
imparikh <parikh.i.m@gmail.com>
2016-11-14 14:33:45 -0500

    making it executable and an attempt to scrape links

--- index.js
+++ index.js
@@ -1,59 +1,74 @@
 var my_info = require('./config');
-var page = new WebPage(), testindex = 0, loadInProgress = false;
+var page = new WebPage();
+var state = 0,
+    loadInProgress = false;
+var OPEN_PAGE = 0,
+    FILL_FORM = 1,
+    LOG_IN = 2,
+    GET_HTML = 3;
 
 page.onConsoleMessage = function(msg) {
-  console.log(msg);
+    console.log(msg);
 };
 
 page.onLoadStarted = function() {
-  loadInProgress = true;
-  console.log("load started");
+    loadInProgress = true;
+    console.log("load started");
 };
 
 page.onLoadFinished = function() {
-  loadInProgress = false;
-  console.log("load finished");
+    loadInProgress = false;
+    console.log("load finished");
 };
 
 var steps = [
-  function() {
-    //Load Login Page
+function() {
     page.open("https://grades.cs.umd.edu/classWeb/login.cgi");
-    console.log("Opened grades page.")
-  },
-  function() {
-    //Enter Credentials
-    page.evaluate(function(my_info) {
-
-      var arr = document.getElementsByTagName("form");
-      arr[0].elements["user"].value = my_info.username;
-      arr[0].elements["password"].value = my_info.password;
-      console.log("Filling out info.");
-    }, my_info);
-  },
-  function() {
+    console.log("Opened grades page.");
+},
+function(user, pass) {
+    var obj = {username: user, password: pass};
+    page.evaluate(function(obj) {
+        console.log("Filling out info.");
+        var arr = document.getElementsByTagName("form");
+        arr[0].elements["user"].value = obj.username;
+        arr[0].elements["password"].value = obj.password;
+    }, obj);
+},
+function() {
     page.evaluate(function() {
-      document.getElementsByTagName("form")[0].submit.click();
-      console.log("Logging in..");
+        document.getElementsByTagName("form")[0].submit.click();
+        console.log("Logging in..");
     });
-  },
-  function() {
+},
+function() {
     page.evaluate(function() {
-      console.log(document.querySelectorAll('html')[0].outerHTML);
-
+        return [].map.call(document.querySelectorAll('a'), function(link) {
+            return link.getAttribute('href');
+        });
     });
-  }
+}
 ];
 
-// Calls all steps 50 ms apart
-interval = setInterval(function() {
-  if (!loadInProgress && typeof steps[testindex] == "function") {
-    console.log("step " + (testindex + 1));
-    steps[testindex]();
-    testindex++;
-  }
-  if (typeof steps[testindex] != "function") {
-    console.log("test complete!");
-    phantom.exit();
-  }
-}, 50);
+function execute(user, pw, classes) {
+    interval = setInterval(function() {
+        if (!loadInProgress && typeof steps[state] == "function") {
+            if (state == OPEN_PAGE) {
+                steps[OPEN_PAGE]();
+            } else if (state == FILL_FORM) {
+                steps[FILL_FORM](user, pw);
+            } else if (state == LOG_IN) {
+                steps[LOG_IN]();
+            } else if (state == GET_HTML){
+                console.log(steps[GET_HTML]().join('\n'));
+            }
+            state++;
+        }
+        if (typeof steps[state] != "function") {
+            console.log("test complete!");
+            phantom.exit();
+        }
+    }, 100);
+}
+
+execute(my_info.username, my_info.password, null);
