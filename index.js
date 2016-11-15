f33fe1d14849a78882a09783cd3a7f64d93a728d
imparikh <parikh.i.m@gmail.com>
2016-11-15 18:28:07 -0500

    no more stps stuff

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ var OPEN_PAGE = 0,
     FILL_FORM = 1,
     LOG_IN = 2,
     GET_HTML = 3,
-    GET_COURSES = 4;
+    GET_COURSES = 4,
+    GET_GRADE = 5;
 
 page.onConsoleMessage = function(msg) {
     console.log(msg);
@@ -22,27 +23,31 @@ page.onLoadFinished = function() {
     console.log("load finished");
 };
 
-var steps = [
-function() {
-    page.open("https://grades.cs.umd.edu/classWeb/login.cgi");
-    console.log("Opened grades page.");
-},
-function(user, pass) {
+function openPage() {
+    page.open("https://grades.cs.umd.edu/classWeb/login.cgi", function(status){
+        if (status === 'success'){
+            console.log("Opened grades page.");
+            fillForm(my_info.username, my_info.password);
+        }
+    });
+}
+
+function fillForm(user, pass) {
     var obj = {username: user, password: pass};
     page.evaluate(function(obj) {
         console.log("Filling out info.");
         var arr = document.getElementsByTagName("form");
         arr[0].elements["user"].value = obj.username;
         arr[0].elements["password"].value = obj.password;
-    }, obj);
-},
-function() {
-    page.evaluate(function() {
         document.getElementsByTagName("form")[0].submit.click();
-        console.log("Logging in..");
-    });
-},
-function(classes) {
+    }, obj);
+    setTimeout(function(){
+        getLinks(['330', '351']);
+    }, 3000);
+}
+
+function getLinks(classes) {
+    console.log("Getting links");
     var linkHash = page.evaluate(function(classes) {
         var newList = {};
         var lst = document.getElementsByTagName("a");
@@ -52,50 +57,53 @@ function(classes) {
                 var curr = lst[i].innerHTML;
                 if (curr.indexOf(classNo) != -1){
                     newList[classNo] = lst[i].href
+                    console.log(lst[i].href);
                     break;
                 }
             }
         })
         return newList;
     }, classes);
-    return linkHash;
+    getGrades(linkHash);
 }
-];
+
+function getGrades(links){
+    var i = 0;
+    var limit = Object.keys(links).length;
+    var id = setInterval(function(){
+        links[Object.keys(links)[i]] = getGradesForPage(i, links);
+        i ++;
+        if (i == limit){
+            clearInterval(id);
+            // AT THIS POINT, LINKS HAS WHAT WE WANT IT TO HAVE
+        }
+    }, 5000);
+
+}
+
+function getGradesForPage(current, links){
+    var arr = Object.keys(links);
+    return page.open(links[arr[current]], function(status){
+        if (status === 'success'){
+            return getGradeOnPage();
+        }
+    });
+}
+
+function getGradeOnPage(){
+    return page.evaluate(function(){
+        var arr = document.getElementsByTagName('td');
+        console.log("Got tds, here is the last element we want:")
+        console.log(arr[arr.length - 3].innerHTML)
+        return arr[arr.length - 3].innerHTML;
+    });
+}
+
 
 
 function execute(user, pw, classes) {
-    interval = setInterval(function() {
-        if (!loadInProgress && typeof steps[state] == "function") {
-            if (state == OPEN_PAGE) {
-                steps[OPEN_PAGE]();
-            } else if (state == FILL_FORM) {
-                steps[FILL_FORM](user, pw);
-            } else if (state == LOG_IN) {
-                steps[LOG_IN]();
-            } else if (state == GET_HTML){
-                links = steps[GET_HTML](classes);
-                /* Uncomment to print */
-                /*
-                Object.keys(links).forEach(function(hell){
-                    console.log(hell + "\n");
-                    console.log(links[hell]);
-                });
-                */
-            } else if (state == GET_COURSES){
-                /* We need to convert a hash that maps
-                    330 -> link to page
-                    to
-                    330 -> 92.14
-                */
-            }
-            state++;
-        }
-        if (typeof steps[state] != "function") {
-            console.log("test complete!");
-            phantom.exit();
-        }
-    }, 100);
+    openPage();
 }
 
 
-execute(my_info.username, my_info.password, ["330", "421"]);
+execute(my_info.username, my_info.password, ["330", "351"]);
