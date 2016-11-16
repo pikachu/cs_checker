d291bc945564925b002dabd2a1359153828e85cc
Michael Stevens <mls0.5mv2@gmail.com>
2016-11-16 18:48:13 -0500

    updated database schema

--- migrations/1.js
+++ migrations/1.js
@@ -5,12 +5,20 @@ exports.up = function(knex) {
       table.increments('id').primary();
       table.string('username');
       table.string('password');
+      table.string('directoryId');
+      table.string('directoryPass');
+      table.string('email');
       table.string('phone_number');
-      table.specificType('courses', 'varchar(255)[]')
+    })
+    .createTable('grades', function(table) {
+        table.bigInteger('id');
+        table.integer('courseCode');
+        table.float('grade');
     });
 };
 
 exports.down = function(knex) {
   return knex.schema
+    .dropTable('grades')
     .dropTable('users');
 };
