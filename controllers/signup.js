7475257d9850aded88e8e26489de6e47a3a988f5
Michael Stevens <mls0.5mv2@gmail.com>
2016-11-26 16:53:50 -0500

    error handling

--- controllers/signup.js
+++ controllers/signup.js
@@ -1,5 +1,6 @@
 var User = require('../models/user');
 var Grade = require('../models/grade');
+var bookshelf = require('../bookshelf');
 var nodemailer = require('nodemailer');
 var auth = require('../public/js/authentication');
 var exec = require('child_process').exec;
@@ -22,44 +23,50 @@ exports.signupPost = function(req, res) {
     });
     var email = req.body.email;
     var password = req.body.password;
-    verifyUser(req.body.umdusername, req.body.umdpass, function(shouldContinue){
-        if (shouldContinue){
-            auth.hash(password, function (err, salt, hash) {
-                if (err) throw err;
-                var user = new User({
-                    email: email,
-                    password_salt: salt,
-                    password_hash: hash.toString('base64'),
-                    phone_number: req.body.phoneNumber,
-                    directory_id: req.body.umdusername,
-                    directory_pass: req.body.umdpass
-                }).save().then(function(newUser) {
+    bookshelf.knex('users').where('email', email).orWhere('directory_id', req.body.umdusername).then(function(user) {
+        if (user.length > 0) {
+            req.flash('error', {msg: 'That user already exists!'});
+            res.redirect('/signup');
+        }
+        verifyUser(req.body.umdusername, req.body.umdpass, function(shouldContinue){
+            if (shouldContinue){
+                auth.hash(password, function (err, salt, hash) {
                     if (err) throw err;
-                    var id = newUser.get('id');
-                    courses.forEach(function(course) {
-                        new Grade({
-                            user_id: id,
-                            course_code: course,
-                            grade: 0.0
-                        }).save();
-                    });
-                    auth.authenticate(newUser.get('email'), password, function(err, user){
-                        if(user){
-                            req.session.regenerate(function(){
-                                req.session.user = user;
-                                req.flash('success', { msg: 'Information saved for ' + req.body.email });
-                                res.redirect('/signup');
-                            });
-                        }
+                    var user = new User({
+                        email: email,
+                        password_salt: salt,
+                        password_hash: hash.toString('base64'),
+                        phone_number: req.body.phoneNumber,
+                        directory_id: req.body.umdusername,
+                        directory_pass: req.body.umdpass
+                    }).save().then(function(newUser) {
+                        if (err) throw err;
+                        var id = newUser.get('id');
+                        courses.forEach(function(course) {
+                            new Grade({
+                                user_id: id,
+                                course_code: course,
+                                grade: 0.0
+                            }).save();
+                        });
+                        auth.authenticate(newUser.get('email'), password, function(err, user){
+                            if(user){
+                                req.session.regenerate(function(){
+                                    req.session.user = user;
+                                    req.flash('success', { msg: 'Information saved for ' + req.body.email });
+                                    res.redirect('/profile');
+                                });
+                            }
+                        });
                     });
                 });
-            });
-        } else {
-            req.session.regenerate(function(){
-                req.flash('success', { msg: 'Incorrect login for ' + req.body.umdusername });
-                res.redirect('/signup');
-            });
-        }
+            } else {
+                req.session.regenerate(function(){
+                    req.flash('success', { msg: 'Incorrect login for ' + req.body.umdusername });
+                    res.redirect('/signup');
+                });
+            }
+        });
     });
 };
 
