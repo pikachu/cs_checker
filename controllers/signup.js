14f01fde460724a1f2e4f1ffb69c05891cc2d61b
imparikh <parikh.i.m@gmail.com>
2016-11-26 19:59:07 -0500

    temp

--- controllers/signup.js
+++ controllers/signup.js
@@ -1,6 +1,5 @@
 var User = require('../models/user');
 var Grade = require('../models/grade');
-var bookshelf = require('../bookshelf');
 var nodemailer = require('nodemailer');
 var auth = require('../public/js/authentication');
 var exec = require('child_process').execSync;
@@ -23,58 +22,62 @@ exports.signupPost = function(req, res) {
     });
     var email = req.body.email;
     var password = req.body.password;
-    bookshelf.knex('users').where('email', email).orWhere('directory_id', req.body.umdusername).then(function(user) {
-        if (user.length > 0) {
-            req.flash('error', {msg: 'That user already exists!'});
-            res.redirect('/signup');
-        }
-        verifyUser(req.body.umdusername, req.body.umdpass, function(shouldContinue){
-            if (shouldContinue){
-                auth.hash(password, function (err, salt, hash) {
+    verifyUser(req.body.umdusername, req.body.umdpass, function(shouldContinue){
+        if (shouldContinue){
+            auth.hash(password, function (err, salt, hash) {
+                if (err) throw err;
+                var user = new User({
+                    email: email,
+                    password_salt: salt,
+                    password_hash: hash.toString('base64'),
+                    phone_number: req.body.phoneNumber,
+                    directory_id: req.body.umdusername,
+                    directory_pass: req.body.umdpass
+                }).save().then(function(newUser) {
                     if (err) throw err;
-                    var user = new User({
-                        email: email,
-                        password_salt: salt,
-                        password_hash: hash.toString('base64'),
-                        phone_number: req.body.phoneNumber,
-                        directory_id: req.body.umdusername,
-                        directory_pass: req.body.umdpass
-                    }).save().then(function(newUser) {
-                        if (err) throw err;
-                        var id = newUser.get('id');
-                        courses.forEach(function(course) {
-                            new Grade({
-                                user_id: id,
-                                course_code: course,
-                                grade: 0.0
-                            }).save();
-                        });
-                        auth.authenticate(newUser.get('email'), password, function(err, user){
-                            if(user){
-                                req.session.regenerate(function(){
-                                    req.session.user = user;
-                                    req.flash('success', { msg: 'Information saved for ' + req.body.email });
-                                    res.redirect('/profile');
-                                });
-                            }
-                        });
+                    var id = newUser.get('id');
+                    // Need to swap this out for functionality in dboard.js
+                    // addNewCourses(id, courses);
+                    courses.forEach(function(course) {
+                        new Grade({
+                            user_id: id,
+                            course_code: course,
+                            grade: 0.0
+                        }).save();
+                    });
+                    auth.authenticate(newUser.get('email'), password, function(err, user){
+                        if(user){
+                            req.session.regenerate(function(){
+                                req.session.user = user;
+                                req.flash('success', { msg: 'Information saved for ' + req.body.email });
+                                res.redirect('/signup');
+                            });
+                        }
                     });
                 });
-            } else {
-                req.session.regenerate(function(){
-                    req.flash('success', { msg: 'Incorrect login for ' + req.body.umdusername });
-                    res.redirect('/signup');
-                });
-            }
-        });
+            });
+        } else {
+            req.session.regenerate(function(){
+                req.flash('success', { msg: 'Incorrect login for ' + req.body.umdusername });
+                res.redirect('/signup');
+            });
+        }
     });
 };
 
 function verifyUser(user, pass, callback){
-    var callStr = 'node ./phantom_scripts/testLogin.js ' + user + ' ' + pass;
+    var callStr = 'node testLogin.js ' + user + ' ' + pass;
     console.log("Trying to verify user " + user);
-    exec(callStr, function(error, stdout, stderr) {
+    exec('pwd', function(error, stdout, stderr) {
+        console.log(stdout);
+      // work with result
+    });
+    /*
+    exec(callStr, {cwd: '../phantom_scripts/'}, function(error, stdout, stderr) {
         console.log(stderr);
         callback(stderr.indexOf("ERROR LOGGING IN") == -1);
     });
+    */
 }
+
+verifyUser('iparikh', 'Helloworld12', null);
