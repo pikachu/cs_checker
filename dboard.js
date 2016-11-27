87b5515dc11101449c25f4136e912807883e728c
imparikh <parikh.i.m@gmail.com>
2016-11-27 00:28:33 -0500

    reorganization of phantom_scripts + linter fixes in dboard

--- dboard.js
+++ dboard.js
@@ -1,17 +1,16 @@
-var User = require('./models/user');
-var Grade = require('./models/grade');
-var bookshelf = require('./bookshelf');
-var testValidClasses = require('./phantom_scripts/testClasses').testValidClasses;
+const Grade = require('./models/grade');
+const bookshelf = require('./bookshelf');
+const testValidClasses = require('./phantom_scripts/testClasses').testValidClasses;
 
 /*
  * newCourses is an array of new courses that we have no information about, but
  * the user wants to monitor them.
  */
-function addCourses(user_id, newCourses){
-    newCourses.forEach(function(course_code){
+function addCourses(userId, newCourses) {
+    newCourses.forEach(courseCode => {
         new Grade({
-            user_id: user_id,
-            course_code: course_code,
+            user_id: userId,
+            course_code: courseCode,
             grade: 0.0
         }).save();
     });
@@ -21,54 +20,51 @@ function addCourses(user_id, newCourses){
  * coursesToDel are courses that the user has previously tracked, but has removed
  * from their desired "to track" list.
  */
-function delCourses(user_id, coursesToDel){
-    coursesToDel.forEach(function(course){
+function delCourses(userId, coursesToDel) {
+    coursesToDel.forEach(course => {
         console.log("Deleting course " + course);
         bookshelf.knex('grades').where({
             course_code: course,
-            user_id: user_id
+            user_id: userId
         }).del().then();
     });
 }
 
 // detectDiffCourses
-function detectDiffCourses(user_id, course_string){
-    var new_courses = course_string.split(",");
-    var old_courses = [];
-    bookshelf.knex('grades').where('user_id', user_id).then(function(grades){
+function detectDiffCourses(userId, courseString) {
+    const newCourses = courseString.split(',');
+    const oldCourses = [];
+    bookshelf.knex('grades').where('user_id', userId).then(grades => {
         console.log("Getting old grades");
-        var add = [];
-        var del = [];
-        grades.forEach(function(grade){
-            old_courses.push(grade.course_code);
+        const add = [];
+        const del = [];
+        grades.forEach(grade => {
+            oldCourses.push(grade.course_code);
         });
         console.log("Current grades in DB");
         console.log(grades);
-        new_courses.forEach(function(new_course){
-            if (!old_courses.includes(new_course)){
-                add.push(new_course);
+        newCourses.forEach(newCourse => {
+            if (!oldCourses.includes(newCourse)) {
+                add.push(newCourse);
             }
         });
-        old_courses.forEach(function(old_course){
-            if (!new_courses.includes(old_course)){
-                del.push(old_course);
+        oldCourses.forEach(oldCourse => {
+            if (!newCourses.includes(oldCourse)) {
+                del.push(oldCourse);
             }
         });
-        delCourses(user_id, del);
-        addCourses(user_id, add);
+        delCourses(userId, del);
+        addCourses(userId, add);
     });
 }
 
 /* Takes an id as the first input */
-function areCoursesValidForUser(user_id, course_string){
-    console.log("Checking if courses are valid for user");
-    testValidClasses(user_id, course_string, function(res){
+function areCoursesValidForUser(userId, courseString) {
+    console.log('Checking if courses are valid for user');
+    testValidClasses(userId, courseString, res => {
         console.log(res);
         return res;
     });
 }
 
-module.exports = {
-    addCourses: addCourses,
-    delCourses: delCourses
-}
+module.exports = { addCourses, delCourses, detectDiffCourses, areCoursesValidForUser };
