2c464bb903022ab570766ac802b9433840898ce3
pikachu <cameron.payton7@gmail.com>
2017-02-01 17:45:41 -0500

    Added change pw functionality

--- controllers/changePass.js
+++ controllers/changePass.js
@@ -20,71 +20,54 @@ function changePassGet(req, res) {
 }
 
 async function changePassPost(req, res) {
+    var nonmatch = false;
+
     if (!req.body.pass || !req.body.newPass || !req.body.newPassConfirm) {
         res.render('changePass', {
-            errorStatus: 'Please enter all information!'
+            passStatus: 'Please enter all information!'
         });
         /* user did not input all info */
         return;
     }
-    /* Makes sure input pass is same as saved pass */
 
     if (req.body.newPassConfirm !== req.body.newPass) {
         res.render('changePass', {
-            errorStatus: 'Passwords do not match!'
+            passStatus: 'Passwords do not match!'
         });
+        return;
         /* pw confirm is wrong, need to render bad */
     }
-    auth.hash(req.body.newPass, (err, salt, hash) => {
-        if (err) throw err;
-        bookshelf.knex('users').where({
-            email: req.body.email,
-            directory_id: req.session.user.id
-        }).update({
-            password_salt: salt.toString('base64'),
-            password_hash: hash.toString('base64')
-        }).then(() => {
-            bookshelf.knex('users').where('id', req.session.user.id).then(users => {
-                req.session.regenerate(() => {
-                    req.session.user = users[0];
-                    res.status(200);
-                    req.flash('success', { msg: `Password changed for ${req.body.email}` });
-                    res.redirect('/profile');
+
+    auth.authenticate(req.session.user.email, req.body.pass, (err, user) => {
+        if (user) {
+            console.log('correct password.');
+            auth.hash(req.body.newPass, (err, salt, hash) => {
+                if (err) throw err;
+                bookshelf.knex('users').where({
+                    email: req.session.user.email,
+                    directory_id: req.session.user.directory_id
+                }).update({
+                    password_salt: salt.toString('base64'),
+                    password_hash: hash.toString('base64')
+                }).then((users) => {
+                    bookshelf.knex('users').where('id', req.session.user.id).then(users => {
+                        req.session.regenerate(() => {
+                            req.session.user = users[0];
+                            res.status(200);
+                            req.flash('success', { msg: `Password changed for ${req.body.email}` });
+                            res.render('changePass', {
+                                passStatus: 'Password changed!'
+                            });
+                        });
+                    });
                 });
             });
-        });
-    });
-}
-
-async function updateProfile(req, res) {
-    if (req.body.newUMDPass && req.body.newUMDPass !== '') {
-        const encryptedPass = encryption.encrypt(req.body.newUMDPass);
-        await bookshelf.knex('users').where('id', req.session.user.id).update({
-            directory_pass: encryptedPass
-        });
-        try {
-            await script.loginToGradeServer(req.session.user.directory_id, req.body.newUMDPass);
-            await bookshelf.knex('users').where('id', req.session.user.id).update({
-                validCredentials: true
-            });
-        } catch (e) {
-            await bookshelf.knex('users').where('id', req.session.user.id).update({
-                validCredentials: false
+        } else {
+            console.log('incorrect password.');
+            res.render('changePass', {
+                passStatus: 'Invalid Password!'
             });
         }
-    }
-    await script.checkUser(req.session.user, false);
-    await bookshelf.knex('users').where('id', req.session.user.id).update({
-        getsEmails: req.body.getsEmails ? true : false,
-        getsTexts: req.body.getsTexts ? true : false,
-        phone_number: phone(req.body.newPhone, 'USA')[0]
-    });
-    const users = await bookshelf.knex('users').where('id', req.session.user.id);
-    req.session.regenerate(() => {
-        req.session.user = users[0];
-        res.status(200);
-        req.flash('success', { msg: `Information saved for ${req.body.email}` });
-        res.redirect('/profile');
     });
 }
 
